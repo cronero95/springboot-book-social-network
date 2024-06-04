1a435c32b5f88b0ca1ce8edc87a411ab5b88b79e
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-06-04 17:57:46 -0500

    initial commit

---
+++ src/main/java/com/springbootprojects/booknetworkapi/BooknetworkapiApplication.java
@@ -0,0 +1,13 @@
+package com.springbootprojects.booknetworkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooknetworkapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooknetworkapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootprojects/booknetworkapi/BooknetworkapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootprojects.booknetworkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooknetworkapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
