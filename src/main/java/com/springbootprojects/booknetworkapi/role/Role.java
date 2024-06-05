f8b563b0335e6aaf442ad5232388a7435d8a3c1c
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-06-05 13:23:34 -0500

    feat: create Role entity

---
+++ src/main/java/com/springbootprojects/booknetworkapi/role/Role.java
@@ -0,0 +1,55 @@
+package com.springbootprojects.booknetworkapi.role;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+@Entity
+@Table(name = "roles")
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.AUTO
+    )
+    private Integer id;
+
+    @Column(
+        unique = true
+    )
+    private String name;
+
+    @CreatedDate
+    @Column(
+        nullable = false,
+        updatable = false
+    )
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(
+        insertable = false
+    )
+    private LocalDateTime lastModifiedDate;
+}
