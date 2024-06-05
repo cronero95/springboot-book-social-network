94235727ce3fee03558b594fd2f4762062b26d72
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-06-05 13:13:24 -0500

    feat: enable JPA auditing

--- src/main/java/com/springbootprojects/booknetworkapi/BooknetworkapiApplication.java
+++ src/main/java/com/springbootprojects/booknetworkapi/BooknetworkapiApplication.java
@@ -2,8 +2,10 @@ package com.springbootprojects.booknetworkapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BooknetworkapiApplication {
 
 	public static void main(String[] args) {
