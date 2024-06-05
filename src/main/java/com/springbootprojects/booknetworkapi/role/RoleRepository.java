3045dd2e8e12fd4e8feb9a2cc524900f497e0496
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-06-05 15:12:52 -0500

    feat: create repositories for User and Role

---
+++ src/main/java/com/springbootprojects/booknetworkapi/role/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.springbootprojects.booknetworkapi.role;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String name);
+}
