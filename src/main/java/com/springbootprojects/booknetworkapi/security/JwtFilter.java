e6fec7526031754bf8a4257b0aa7752bcb3bb53b
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-06-05 22:30:29 -0500

    feat: create class to filter request

---
+++ src/main/java/com/springbootprojects/booknetworkapi/security/JwtFilter.java
@@ -0,0 +1,47 @@
+package com.springbootprojects.booknetworkapi.security;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain
+    ) throws ServletException, IOException {
+        
+        if(request.getServletPath().contains("/api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String jwt;
+        final String userEmail;
+
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUserEmail(jwt);
+    }
+
+}
