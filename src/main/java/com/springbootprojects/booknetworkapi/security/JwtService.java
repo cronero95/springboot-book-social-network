2c25c05993b7caff391f2ef5bc81b1925a79030d
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-06-06 22:11:41 -0500

    feat: create methods for token validation

--- src/main/java/com/springbootprojects/booknetworkapi/security/JwtService.java
+++ src/main/java/com/springbootprojects/booknetworkapi/security/JwtService.java
@@ -4,12 +4,14 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
@@ -57,13 +59,49 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean isTokenValid(
+        String token,
+        UserDetails userDetails
+    ) {
+        final String userEmail = extractUserEmail(token);
+
+        return userEmail.equals(userDetails.getUsername())
+            && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public String extractUserEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(
+        String token,
+        Function<Claims, T> claimResolver
+    ) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(encodedSecretKey);
 
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public String extractUserEmail(String jwt) {
-        return "";
-    }
 }
